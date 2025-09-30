296902311f2d370fe33f395b7b4c60dda0725ade
stavroula <nika.stavroula@gmail.com>
2025-09-30 17:44:01 +0100

    initial commit

---
+++ src/components/Gig.jsx
@@ -0,0 +1,27 @@
+//Create a component called Gig, which contains information about a gig on the website. It should contain:
+
+//A h3 (heading) element listing the name of the band
+//A img (image) element for an image of the band, using the src attribute to link to an image.
+//A p (paragraph) element containing a description of the event
+//A p element containing the time and date of the event
+//A p element containing the location of the event
+
+
+import React from "react";
+
+const Gig = () => {
+    return (
+        <div className="Gig">
+            <h3>Fleetwood Mac</h3>
+            <img 
+                src="https://yt3.googleusercontent.com/pTa9CSj4p2bCopIiKZDhYV5ABd2YYhtEfh7bAB3FL0G8SfGj6cTUAtdjXdsXpGNOKpHnMv1z2g=s900-c-k-c0x00ffffff-no-rj" 
+                alt="Fleetwood Mac performing live" 
+            />
+            <p id="Description">A tribute concert featuring the band's most-loved songs, performed by a live band with a focus on capturing the iconic sound and emotion of the original group's golden era.</p>
+            <p id="TimeDate">20:00 25/10/2026</p>
+            <p id="Location">Royal Albert Hall, Kensington Gore, South Kensington, London, SW7 2AP</p>
+        </div>
+    );
+};
+
+export default Gig;
